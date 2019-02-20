8bf40ff316cd1eea2270ba58cd6b1fc13d78c74e
fabian1103724 <fabian1103724@ceu.edu.ph>
2019-02-20 14:29:21 +0800

    Finals

---
+++ Draw.java
@@ -0,0 +1,174 @@
+import javax.swing.JComponent;
+import javax.swing.Timer;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.net.URL;
+import java.util.Random;
+
+
+public class Draw extends JComponent{
+
+	public BufferedImage image;
+	public BufferedImage backgroundImage;
+	public URL resource = getClass().getResource("run0.png");
+
+	
+	
+	// circle's position
+	public int x = 300;
+	public int y = 300;
+	public int height = 0;
+	public int width = 0;
+	
+
+	// animation states
+	public int state = 0;
+
+	// randomizer
+	public Random randomizer;
+
+	// enemy
+	public int enemyCount;
+	Monster[] monsters = new Monster[10];
+
+
+	public Hero hero1;
+	public Draw(){
+		randomizer = new Random();
+		spawnEnemy();
+		hero1 = new Hero(this);
+		
+		try{
+			image = ImageIO.read(resource);
+			backgroundImage = ImageIO.read(getClass().getResource("background.jpg"));
+		}
+		catch(IOException e){
+			e.printStackTrace();
+		}
+
+		height = image.getHeight();
+		width = image.getWidth();
+
+		startGame();
+	}
+
+	public void startGame(){
+		Thread gameThread = new Thread(new Runnable(){
+			public void run(){
+				while(true){
+					try{
+						for(int c = 0; c < monsters.length; c++){
+							if(monsters[c]!=null){
+								monsters[c].moveTo(x,y);
+								repaint();
+							}
+						}
+						Thread.sleep(100);
+					} catch (InterruptedException e) {
+							e.printStackTrace();
+					}
+				}
+			}
+		});
+		gameThread.start();
+	}
+
+	public void spawnEnemy(){
+		if(enemyCount < 10){
+			monsters[enemyCount] = new Monster(randomizer.nextInt(500), randomizer.nextInt(500), this);
+			enemyCount++;
+		}
+	}
+	public void checkCollision(){
+		int xChecker = hero1.x + width;
+		int yChecker = hero1.y;
+
+		for(int x=0; x<monsters.length; x++){
+			boolean collideX = false;
+			boolean collideY = false;
+
+			if(monsters[x]!=null){
+				monsters[x].contact = false;
+
+				if(yChecker > monsters[x].yPos){
+					if(yChecker-monsters[x].yPos < monsters[x].height){
+						collideY = true;
+					}
+				}
+				else{
+					if(monsters[x].yPos - yChecker < monsters[x].height){
+						collideY = true;
+					}
+				}
+
+				if(xChecker > monsters[x].xPos){
+					if(xChecker-monsters[x].xPos < monsters[x].width){
+						collideX = true;
+					}
+				}
+				else{
+					if(monsters[x].xPos - xChecker < 5){
+						collideX = true;
+					}
+				}
+			}
+
+			if(collideX && collideY){
+				System.out.println("collision!");
+				monsters[x].contact = true;
+			}
+		}
+	}
+
+
+	
+	public void paintComponent(Graphics g){
+		super.paintComponent(g);
+
+		
+				g.drawImage(hero1.image, hero1.xPos, hero1.yPos, this);
+				
+				g.drawImage(backgroundImage, 0, 0, this);
+				
+				g.setColor(Color.BLACK);
+				g.fillRect(0, 480, 600, 100);
+				g.setColor(Color.GREEN);
+				g.fillRect(0, 500, hero1.life, 10);
+				g.setColor(Color.WHITE);
+				g.drawString("Hero Life",0,495);
+
+				g.setColor(Color.BLUE);
+				g.fillRect(490, 480, 50, 30);
+				g.setColor(Color.WHITE);
+				g.drawString("Items",500,495);
+
+
+				g.setColor(Color.BLUE);
+				g.fillRect(490, 530, 50, 30);
+				g.setColor(Color.WHITE);
+				g.drawString("Menu",500,550);
+
+
+
+
+		// character grid for hero
+		// g.setColor(Color.YELLOW);
+		// g.fillRect(x, y, width, height);
+		g.drawImage(hero1.image, hero1.x, hero1.y, this);
+		
+
+		for(int c = 0; c < monsters.length; c++){
+			if(monsters[c]!=null){
+				// character grid for monsters
+				// g.setColor(Color.BLUE);
+				// g.fillRect(monsters[c].xPos, monsters[c].yPos+5, monsters[c].width, monsters[c].height);
+				g.drawImage(monsters[c].image, monsters[c].xPos, monsters[c].yPos, this);
+				g.setColor(Color.GREEN);
+				g.fillRect(monsters[c].xPos+7, monsters[c].yPos, monsters[c].life, 3);
+			}	
+		}
+	}
+}
